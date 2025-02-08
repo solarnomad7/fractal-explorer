890ec3c9611b7f3ba78ebb17c46614bb9f8c626b
Priit Põder <185842118+solarnomad7@users.noreply.github.com>
2025-02-08 21:55:45 +0200

    Update default.lua

--- scripts/default.lua
+++ scripts/default.lua
@@ -1,7 +1,16 @@
+local MAX <const> = 255
+
+-- Iterates over the palette (i = 0->maxIterations+1)
+-- Returns an RGB value and inserts it into the palette at index i
 function setPaletteColor(i, maxIterations)
-    r = 0
-    b = 255 * (i / maxIterations)
-    g = 0
+    r, g, b = 0, 0, 0
+
+    -- Check if pixel is outside the set
+    if i < maxIterations then
+        r = 0
+        g = MAX * (i / maxIterations)
+        b = 0
+    end
 
     return r, g, b
 end
